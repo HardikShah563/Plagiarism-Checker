bf3469876f45e035a6cb8cc149d48879795d523b
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-07-06 11:10:12 +0530

    Add files via upload

---
+++ assets/backend/plagiarism-checker.py
@@ -0,0 +1,19 @@
+import sys
+data_to_pass = 'Send this string to javascript file'
+input = sys.argv[1]
+output = data_to_pass
+print(output)
+sys.stdout.flush()
+
+
+
+
+from difflib import SequenceMatcher #importing SequenceMatcher from difflib library to perform plagiarism operations
+def Plagiarism_Checker(file1_data, file2_data): 
+    similarity = SequenceMatcher(None, file1_data, file2_data).ratio()
+    return (similarity*100)
+
+str1 = input("Enter doc1: ")
+str2 = input("Enter doc2: ")
+final = Plagiarism_Checker(str1, str2)
+print("Similarity: ", final, "%")
